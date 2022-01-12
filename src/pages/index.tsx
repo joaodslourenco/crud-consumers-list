9d877b82f359b7d43eb67f9a7602c405d6387e9e
Joao Lourenco <joaodslourenco@gmail.com>
2022-01-12 15:17:19 -0300

    Client class and Table component built

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import Layout from '../components/Layout'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
 
 export default function Home() {
+  const clientes = [
+    new Cliente('1', 'Ana', 23),
+    new Cliente('2', 'Pedro', 26),
+    new Cliente('3', 'Bruna', 27),
+    new Cliente('4', 'Roberto', 59)
+  ]
+
   return (
     <div className="flex h-screen items-center justify-center bg-gradient-to-r from-blue-500 to-purple-500 text-white">
       <Layout titulo="Cadastro simples">
-        <div>conteúdo</div>
+        <Tabela clientes={clientes} />
       </Layout>
     </div>
   )
