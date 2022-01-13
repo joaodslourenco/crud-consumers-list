789798a8d9158eefeb99e6e954413bd76c11618e
Joao Lourenco <joaodslourenco@gmail.com>
2022-01-13 19:34:14 -0300

    implemented base flow for firebase

---
+++ src/backend/db/ColecaoCliente.ts
@@ -0,0 +1,51 @@
+import firebase from '../config'
+import Cliente from '../../core/Cliente'
+import ClienteRepositorio from '../../core/ClienteRepositorio'
+
+export default class ColecaoCliente implements ClienteRepositorio {
+  #conversor = {
+    toFirestore(cliente: Cliente) {
+      return {
+        nome: cliente.nome,
+        idade: cliente.idade
+      }
+    },
+    fromFirestore(
+      snapshot: firebase.firestore.QueryDocumentSnapshot,
+      options: firebase.firestore.SnapshotOptions
+    ): Cliente {
+      const dados = snapshot.data(options)
+      return new Cliente(dados.nome, dados.idade, +snapshot.id)
+    }
+  }
+
+  /*dentro do método "salvar", o if verifica se já existe cliente com o id passado. Caso positivo, ele altera os dados através do método set. Caso negativo, ele cria um novo cliente com os dados recebidos. */
+
+  async salvar(cliente: Cliente): Promise<Cliente> {
+    if (cliente?.id) {
+      await this.#colecao().doc(cliente.id).set(cliente)
+      return cliente
+    } else {
+      const docRef = await this.#colecao().add(cliente)
+      const doc = await docRef.get()
+      return doc.data()
+    }
+  }
+
+  async excluir(cliente: Cliente): Promise<void> {
+    return this.#colecao().doc(cliente.id).delete()
+  }
+
+  async obterTodos(): Promise<Cliente[]> {
+    const query = await this.#colecao().get()
+    return query.docs.map(doc => doc.data())
+  }
+
+  /* o método "colecao" aqui é responsável por interagir com o firebase, informando a coleção a ser consumida e o conversor, que é responsável por enviar e receber dados do firebase*/
+  #colecao() {
+    return firebase
+      .firestore()
+      .collection('clientes')
+      .withConverter(this.#conversor)
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Botao from '../components/Botao'
 import Formulario from '../components/Formulario'
 import Layout from '../components/Layout'
 import Tabela from '../components/Tabela'
 import Cliente from '../core/Cliente'
+import ClienteRepositorio from '../core/ClienteRepositorio'
+import ColecaoCliente from '../backend/db/ColecaoCliente'
 
 export default function Home() {
+  const repo: ClienteRepositorio = new ColecaoCliente()
+
   const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes, setClientes] = useState<Cliente[]>([])
   const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
 
-  const clientes = [
-    new Cliente('1', 'Ana', 23),
-    new Cliente('2', 'Pedro', 26),
-    new Cliente('3', 'Bruna', 27),
-    new Cliente('4', 'Roberto', 59)
-  ]
+  useEffect(obterTodos, [])
+
+  function obterTodos() {
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela')
+    })
+  }
 
   function clienteSelecionado(cliente: Cliente) {
     setCliente(cliente)
@@ -31,7 +38,7 @@ export default function Home() {
   }
 
   function salvarCliente(cliente: Cliente) {
-    console.log(cliente)
+    repo.salvar(cliente)
     setVisivel('tabela')
   }
 
